8020bfc0878786467bbc37fc04caa7257a9d6104
Bhasker Kamshetty <30320555+bhaskerkamshetty@users.noreply.github.com>
2024-03-03 20:01:36 +0530

    Update join-domain.bat

--- join-domain.bat
+++ join-domain.bat
@@ -4,4 +4,4 @@ rem #Description: This script joins windows machine into a domain
 rem #Date: 20th February 2024
 
 wmic computersystem where name="%computername%" call joindomainorworkgroup fjoinoptions=3 name="DOMAIN.COM" username="DOMAIN.COM\USERNAME" Password="PASSWORD"
-shutdown -r
+shutdown.exe /r /t 00
