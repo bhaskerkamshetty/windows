d6324ebd20210d60244916bf21664f8390177638
Bhasker Kamshetty <30320555+bhaskerkamshetty@users.noreply.github.com>
2024-09-23 19:38:56 +0530

    Create apps.bat

---
+++ apps.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+rem #Author: Bhasker Kamshetty
+rem #Description: This script installs powershell & powertoys
+rem #Date: 23rd September 2024
+
+rem #Installs PowerShell
+winget install --id Microsoft.PowerShell --source winget
+
+rem #Installs PowerToys
+winget install --id Microsoft.PowerToys --source winget
