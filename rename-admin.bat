dd22d3d3f83a77ec892c0751ce43859fd142e272
Bhasker Kamshetty <30320555+bhaskerkamshetty@users.noreply.github.com>
2024-12-30 00:43:54 +0530

    Update rename-admin.bat

--- rename-admin.bat
+++ rename-admin.bat
@@ -7,12 +7,22 @@ rem #To set custom hostname
 set /p hostname=Enter Hostname: 
 wmic computersystem where name="%computername%" call rename name="DESKTOP-%hostname%"
 
-rem #To set serial number as hostname
+rem #To set serial number as hostname followed by PREDEFINED text
 setlocal
 for /f "tokens=2 delims==" %%a in ('wmic computersystem get name /value') do set "computername=%%a"
 for /f "tokens=2 delims==" %%a in ('wmic bios get serialnumber /value') do set "serialnumber=%%a"
+set "serialnumber=%serialnumber: =%"
 echo Renaming computer from %computername% to %serialnumber%
-wmic computersystem where name="%computername%" call rename name="DESKTOP-%serialnumber%"
+wmic computersystem where name="%computername%" call rename name="PREDEFINED-%serialnumber%"
+endlocal
+
+rem #To set last 4 characters of serial number as hostname followed by PREDEFINED text
+setlocal
+for /f "tokens=2 delims==" %%a in ('wmic computersystem get name /value') do set "computername=%%a"
+for /f "tokens=2 delims==" %%a in ('wmic bios get serialnumber /value') do set "serialnumber=%%a"
+set "serialnumber=%serialnumber: =%" 
+set "short_serial=%serialnumber:~-4%" 
+wmic computersystem where name="%computername%" call rename name="PREDEFINED-%short_serial%"
 endlocal
 
 wmic useraccount where name='Administrator' rename 'NEWUSERNAME'
